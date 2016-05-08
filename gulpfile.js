b323eba9b2d9b751e73f80233f09b16372f9d9de
Alfredo Miranda <alfredo@miranda.io>
2016-05-08 10:35:04 -0500

    Use gulp to optimize build

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('cssnano', function() {
+  return gulp.src('app/css/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('img', function() {
+  return gulp.src('app/img/*.+(png|jpg)')
+    // local machine having some issues installing mozjpeg
+    // .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', function() {
+  runSequence('clean:dist', ['html', 'cssnano', 'img']);
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});

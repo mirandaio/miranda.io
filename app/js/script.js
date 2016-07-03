3a5cd1241bfd1acfda0271e5f2cfd28cb30c2be7
Alfredo Miranda <alfredo@miranda.io>
2016-07-03 16:13:10 -0500

    meh

--- app/js/script.js
+++ app/js/script.js
@@ -21,29 +21,29 @@ window.onload = function() {
       timeNextFrame += 1 / 60;
       
       // update visuals for a single frame
-      vines = vines.filter(v => v.l--);
-      vines.forEach(v => {
-        dx = Math.cos(v.a) * v.w / 2;
-        dy = Math.sin(v.a) * v.w / 2;
-        v.x += dx;
-        v.y += dy;
-        v.a += v.ai / v.w / 2; // base angle increment on thickness of branch
-        v.p.splice(0, v.p.length - v.l);
-        v.p.splice(0, v.p.length - 65); // leave the last 300 points
-        v.p.push({x: v.x, y: v.y}); // add a point to each vine
+      vines = vines.filter(vine => vine.l--);
+      vines.forEach(vine => {
+        dx = Math.cos(vine.a) * vine.w / 2;
+        dy = Math.sin(vine.a) * vine.w / 2;
+        vine.x += dx;
+        vine.y += dy;
+        vine.a += vine.ai / vine.w / 2;
+        vine.p.splice(0, vine.p.length - vine.l);
+        vine.p.splice(0, vine.p.length - 65); // leave the last 300 points
+        vine.p.push({x: vine.x, y: vine.y}); // add a point to each vine
         if(frame % 30 == 0) {
-          v.ai = Math.random() - 0.5;
+          vine.ai = Math.random() - 0.5;
         }
-        if(v.w > 5 && Math.random() < 0.02 && vines.length < 60) {
+        if(vine.w > 5 && Math.random() < 0.02 && vines.length < 60) {
           // position of new vine same as position of current vine
           vines.push({
-            x: v.x,
-            y: v.y,
-            a: v.a,
-            ai: v.ai,
+            x: vine.x,
+            y: vine.y,
+            a: vine.a,
+            ai: vine.ai,
             w: Math.random() * 4 + 3,
             p: [],
-            l: Math.min(4096, 0 | v.w * 32 * (1 + Math.random()))
+            l: Math.min(4096, 0 | vine.w * 32 * (1 + Math.random()))
           });
         }
       });
@@ -55,13 +55,13 @@ window.onload = function() {
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.strokeStyle = '#000';
     ctx.lineWidth = 4;
-    vines.forEach(v => {
-      if(v.w == 8) {
-        ctx.translate(-v.x, -v.y);
+    vines.forEach(vine => {
+      if(vine.w == 8) {
+        ctx.translate(-vine.x, -vine.y);
       }
       ctx.beginPath();
-      l = v.p.length - 1;
-      for(i = l; p = v.p[i]; i--) {
+      l = vine.p.length - 1;
+      for(i = l; p = vine.p[i]; i--) {
         ctx.lineTo(p.x, p.y);
       }
       ctx.stroke();

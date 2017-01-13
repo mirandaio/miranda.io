19b0c60f294d4ca2fd6c5e4cb8f915b1594b33dd
Alfredo Miranda <alfredo@miranda.io>
2017-01-12 20:00:58 -0500

    Set context

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,8 @@
 import { resolve } from 'path';
 
 export default {
-  entry: './src/js/script.js',
+  context: resolve('src'),
+  entry: './js/script.js',
   output: {
     filename: 'bundle.js',
     path: resolve('dist')

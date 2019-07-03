eae0825be3a9d4b3bc087b1d462e4190267db403
Alfredo Miranda <alfredo@miranda.io>
2019-07-03 23:59:25 +0100

    Serve assets from build directory

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const { PORT = 8000 } = process.env;
 
 const app = express();
 
-app.use('/static', express.static('dist/static'));
+app.use('/static', express.static('build/static'));
 
 app.get('/*', (req, res) => {
   res.sendFile('index.html', {
-    root: 'dist'
+    root: 'build'
   });
 });
 

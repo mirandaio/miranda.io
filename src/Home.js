e031de21ed1311a527cb534517fddb6e6d1771c7
Alfredo Miranda <alfredo@miranda.io>
2019-07-19 20:10:23 +0100

    Display image

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import about from './img/about.jpg';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <span>I'm Alfredo.</span>
       </div>
       <p>
+        <img src={about} alt="Profile photo"/>
         I am a software engineer. I enjoy building large custom web applications, and
         helping others. I have a solid computer science background and can tackle difficult
         challenges. My strenghts are developing user interfaces and backend systems. I am

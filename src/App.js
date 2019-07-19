77bceeb5cc0d2b2f644aef3fe733a22f496b2d3f
Alfredo Miranda <alfredo@miranda.io>
2019-07-19 09:04:58 +0100

    Animate canvas

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Projects from './Projects';
 import Resume from './Resume';
 import Contact from './Contact';
+import animateVines from './vines';
 
-const App = () => {
-  return (
-    <Router>
-      <Fragment>
-        <nav>
-          <ul>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/resume">Resume</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-          <canvas/>
-        </nav>
-        <Route exact path="/" component={Home}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/resume" component={Resume}/>
-        <Route path="/contact" component={Contact}/>
-      </Fragment>
-    </Router>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidMount() {
+    animateVines(this.canvasRef.current);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <nav>
+            <ul>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            <canvas ref={this.canvasRef}/>
+          </nav>
+          <Route exact path="/" component={Home}/>
+          <Route path="/projects" component={Projects}/>
+          <Route path="/resume" component={Resume}/>
+          <Route path="/contact" component={Contact}/>
+        </Fragment>
+      </Router>
+    );
+  }
 };
 
 export default App;

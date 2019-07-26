8af430c8ee1f335e6d6fc743c8fc0244c108d101
Alfredo Miranda <alfredo@miranda.io>
2019-07-26 22:22:06 +0100

    Extract Router outside App.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express');
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router';
+import App from './src/App';
 const { PORT = 8000 } = process.env;
 
 const app = express();
@@ -6,6 +10,11 @@ const app = express();
 app.use('/static', express.static('build/static'));
 
 app.get('/*', (req, res) => {
+  const html = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
   res.sendFile('index.html', {
     root: 'build'
   });

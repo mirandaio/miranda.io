95b9574d18bb0b8d0661334d7e8a6ae1b615fa42
Alfredo Miranda <alfredo@miranda.io>
2019-07-27 17:06:56 +0100

    Server rendered React component

--- server.js
+++ server.js
@@ -15,9 +15,22 @@ app.get('/*', (req, res) => {
       <App />
     </StaticRouter>
   );
-  res.sendFile('index.html', {
-    root: 'build'
-  });
+
+  res.send(`
+    <!DOCTYPE>
+    <html>
+    <head>
+      <meta charset="utf-8"/>
+      <title>Alfredo Miranda</title>
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet">
+    </head>
+    <body>
+      <div id="root">
+        ${html}
+      </div>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

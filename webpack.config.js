1c3ae5612d5f156c52f095879d500ae5dba0e560
Alfredo Miranda <alfredo@miranda.io>
2019-07-29 22:35:30 +0100

    Add favicon to html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
-module.exports = ({mode, analyze}) => {
-  const plugins = [ new HtmlWebpackPlugin({ template: 'src/index.html'}) ];
+module.exports = ({ mode, analyze }) => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/img/favicon.png'
+    })
+  ];
 
-  if(analyze) {
+  if (analyze) {
     plugins.push(new BundleAnalyzerPlugin());
   }
 
-  return webpackMerge({
-    mode,
-    module: {
-      rules: [
-        { test: /\.js$/, use: 'babel-loader' },
-        { test: /\.jpe?g$/, use: 'url-loader' }
-      ]
+  return webpackMerge(
+    {
+      mode,
+      module: {
+        rules: [
+          { test: /\.js$/, use: 'babel-loader' },
+          { test: /\.jpe?g$/, use: 'url-loader' }
+        ]
+      },
+      plugins
     },
-    plugins
-  }, require(`./webpack.${mode}`));
+    require(`./webpack.${mode}`)
+  );
 };
